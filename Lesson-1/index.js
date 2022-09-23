940a71008239e925bffe9a41c914609e9844eed2
Ruben <titorenko.ruben@gmail.com>
2022-09-23 16:44:09 +0300

    Made homework-1

---
+++ Lesson-1/index.js
@@ -0,0 +1,86 @@
+// 2.Задание
+let a1  =  5 % 3;
+let a2  =  3 % 5;
+let a3  =  5 + '3'; 
+let a4  =  '5'- 3;
+let a5  =  75 + 'кг';
+let a6  =  785 * 653; 
+let a7  =  100 / 25; 
+let a8  =  0 * 0;
+let a9  =  0 / 2; 
+let a10 =  89 / 0;
+let a11 =  98 + 2; 
+let a12 =  5 - 98; 
+let a13 =  (8 + 56 * 4) / 5;
+let a14 =  (9 - 12) * 7 / (5 + 2); 
+let a15 =  +"123";
+let a16 =  1 || 0;
+let a17 =  false || true; 
+let a18 =  true > 0.
+
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a16, typeof a16);
+console.log(a17, typeof a17);
+console.log(a18, typeof a18);
+
+// 4.Задание
+let heightCilinder = 10;
+let VCilindra = heightCilinder*a7;
+
+console.log(VCilindra)
+
+// 3.Задание
+let width  = 10;
+let height = 23;
+let SPryam = width*height;
+
+console.log(SPryam)
+
+// 5.Задание
+let r = Math.pow(5, 2);
+let SKruga = 3.148*r;
+
+console.log(SKruga);
+
+// 6.Задание
+let a = 5;
+let b = 7;
+let h = 10;
+let STrap = ((a+b)/2)*h
+
+console.log(STrap)
+
+// 7.Задание
+let s = 2000000;
+let p = 10;
+let years = 5;
+let Pereplata = (s/p)*years
+
+console.log(Pereplata)
+
+// // 8.Задание
+// a+2(x-b)=16; a+2x-2b = 16
+a = 8;
+b = 3;
+let x = (16-a+2*b)/2  
+console.log(x)
+
+
+
+
+
+
