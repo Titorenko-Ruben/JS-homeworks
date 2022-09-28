ef8873785d73b3c6ddb78504b60333f157fcc622
Ruben <titorenko.ruben@gmail.com>
2022-09-28 21:10:00 +0300

    Add the task 10

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -1,4 +1,5 @@
 // Задание.1
+
 for(i=1; i<=50; i++) {
     console.log(i);
 }
@@ -22,11 +23,8 @@ while (j>=11 ) {
 // Задание.3
 let b;
 let sum = 0;
-
+console.log('Задание.3');
 for(b=0;b <=100; b++){
-    if(b==0){
-        console.log('Задание.3');
-    }
     sum=sum+b;
     if(b==100){
         console.log(sum);
@@ -35,13 +33,11 @@ for(b=0;b <=100; b++){
 }
 
 //Задание.4
+console.log('Задание.4');
 let g;
 let sum1 = 0;
 
 for(g=1;g<=5;g++){
-    if(g==1){
-        console.log('Задание.4');
-    }
     sum1=sum1+g
     console.log(sum1)
 }
@@ -57,69 +53,67 @@ for(let q=8;q<=56;q++){
 
 //Задание.6
 let sum2;
+document.write('<br>Задание.6<br>')
 
 for(a=2;a<=10; a++){
     for(let j=2; j<=10; j++){
-        if(a==2&&j==2){
-            document.write('<br>Задание.6<br>')
-        }
             sum2=a*j
             document.write(a+'*'+j+'='+sum2+'<br>')
-        if(j==10){
-            document.write('<br>')
+            if(j==10){
+                document.write('<br>')
+            }
         }
     }
-}
-
-//Задание.7
-let n=1000;
-let num=0;
-
-for(n,num; n>50; num++){
-    n/=2
-}
-document.write('Задание.7<br>')
-document.write(n+'<br>');
-document.write(num+'<br>');
-
-
-//Задание.8
-let user = prompt("Введите число, для задания 8.");
-// let user = '100';
-let sum5 = 0;
-let number = 0;
-
-for(let p=0; p < user.length;p++) {
-    sum5=sum5+Number(user[p])
-    number=Number(user)/user.length;
-}
- if(user==0||''){
+    
+    //Задание.7
+    let n=1000;
+    let num=0;
+    
+    for(n,num; n>50; num++){
+        n/=2
+    }
+    document.write('Задание.7<br>')
+    document.write(n+'<br>');
+    document.write(num+'<br>');
+    
+    
+    //Задание.8
     document.write('Задание.8<br>')
-    document.write('Вы ничего не ввели или же ввели 0.')
- }else{
-document.write('Задание.8<br>')
-document.write('Сумма чисел равна = '+sum5+'<br>')
-document.write('Среднее  арифметическое число = '+number)}
-
-//Задание.9
-console.log('Задание.9')
-let str, x, largest, smallest;
-    str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 9';
-    x = 0;
-    largest = 0;
-    smallest = Infinity;
-for(let i = 0; i < str.length - 1; i++) {
-    if(str[i+1] == ' ') {
-        x = str[i];
-    }else {
-        x = str[i] + str[i+1];
+    let user = prompt("Введите число, для задания 8.");
+    // let user = '100';
+    let sum5 = 0;
+    let number = 0;
+    
+    for(let p=0; p < user.length;p++) {
+        sum5=sum5+Number(user[p])
+        number=Number(user)/user.length;
     }
-    if (x >= largest){
-        largest = x;
-    }
-}
-for(let i = 0; i < str.length - 1; i++) {
-    if(str[i+1] == ' ') {
+    if(user==0||''){
+        document.write('Вы ничего не ввели или же ввели 0.')
+    }else{
+        document.write('Задание.8<br>')
+        document.write('Сумма чисел равна = '+sum5+'<br>')
+        document.write('Среднее  арифметическое число = '+number)}
+        
+        //Задание.9
+        console.log('Задание.9')
+        let str, x, largest, smallest;
+        str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 9';
+        x = 0;
+        largest = 0;
+        smallest = Infinity;
+        for(let i = 0; i < str.length - 1; i++) {
+            if(str[i+1] == ' ') {
+                x = str[i];
+            }else {
+                x = str[i] + str[i+1];
+            }
+            if (x >= largest){
+                largest = x;
+            }
+        }
+        for(let i = 0; i < str.length - 1; i++) {
+            if(str[i+1] == ' ') {
         x = str[i];
     }else {
         x = str[i] + str[i+1];
@@ -132,6 +126,22 @@ console.log('Самое большое число в строке = '+largest)
 console.log('Самое маленькое число в строке= '+smallest)
 
 //Задание.10
+document.write('<br>Задание.10<br>')
+
+let p = '963';
+let numb = 0;
+document.write('a. ')
+for(let i=0;i<p.length;i++){
+    document.write(Number(p[i])+ ' ')
+}  
+document.write('<br>'+'b. '+p.length+'<br>')
+for(let i=0;i<=p.length;i++){
+    numb = numb + Number(p[i])
+    if(Number(p[i])== Number(p.length)) {
+        document.write('c. '+numb+'<br>')
+    }   
+} 
+
 
 
 
